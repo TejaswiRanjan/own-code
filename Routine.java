3b4e94865bc650c345fa2a9caa8ddc9400e8bfbb
TejaswiRanjan <ranjantejaswi65@gmail.com>
2024-03-16 21:35:14 +0530

    My Personal Routine In Java Program

---
+++ Routine.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+class Routine{
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no of day(1 to 7) : ");
+        int day = sc.nextInt();
+        System.out.println("Enter the time in integer and float");
+        float time = sc.nextFloat();
+
+        if(day==6&&time>=0&&time<25){
+            System.out.println("Learn revision of generic subject");
+        }else if(day==7&&time>=0&&time<25){
+            System.out.println("Learn and Revision of Operating System and Computer Networking");
+        }else if(day==1&&time>=10&&time<12){
+            System.out.println("Revision 10:00Am to 12:00Am");
+        }else if(day==1&&time>=12&&time<15){
+            System.out.println("DSA(Data Structure and algorithm) 12:00Pm to 3:00Pm");
+        }else if(day==1&&time>=15&&time<16){
+            System.out.println("Rest and Chill 3:00Pm to 4:00Pm");
+        }else if(day==1&&time>=16&&time<19){
+            System.out.println("Web Development 4:00Pm to 7:00Pm");
+        }else if(day==1&&time>=19&&time<20){
+            System.out.println("Rest and Chill 7:00Pm to 8:00Pm");
+        }else if(day==1&&time>=20&&time<22){
+            System.out.println("Trading time 8:00Pm to 10:00Pm");
+        }else if(day==1&&time>=22&&time<23){
+            System.out.println("Trading and eating food 10:00Pm to 11:00Pm");
+        }else if(day==1&&time>=23&&time<24.5){
+            System.out.println("Youtube Knowledge 11Pm to 12:30Am");
+        }else if(day==1&&time>=0.50&&time<2){
+            System.out.println("Conding Practice 12:30Am to 2:00Am");
+        }else if(day==1&&time>=2&&time<10){
+            System.out.println("Rest and all other activity 2:00Am to 10:00Am");
+        }else if(day==2&&time>=10&&time<12){
+            System.out.println("Revision 10:00Am to 12:00Am");
+        }else if(day==2&&time>=12&&time<15){
+            System.out.println("DSA(Data Structure and algorithm) 12:00Pm to 3:00Pm");
+        }else if(day==2&&time>=15&&time<16){
+            System.out.println("Rest and Chill 3:00Pm to 4:00Pm");
+        }else if(day==2&&time>=16&&time<19){
+            System.out.println("Web Development 4:00Pm to 7:00Pm");
+        }else if(day==2&&time>=19&&time<20){
+            System.out.println("Rest and Chill 7:00Pm to 8:00Pm");
+        }else if(day==2&&time>=20&&time<22){
+            System.out.println("Trading time 8:00Pm to 10:00Pm");
+        }else if(day==2&&time>=22&&time<23){
+            System.out.println("Trading and eating food 10:00Pm to 11:00Pm");
+        }else if(day==2&&time>=23&&time<24.5){
+            System.out.println("Youtube Knowledge 11Pm to 12:30Am");
+        }else if(day==2&&time>=0.50&&time<2){
+            System.out.println("Conding Practice 12:30Am to 2:00Am");
+        }else if(day==2&&time>=2&&time<10){
+            System.out.println("Rest and all other activity 2:00Am to 10:00Am");
+        }else if(day==3&&time>=10&&time<12){
+            System.out.println("Revision 10:00Am to 12:00Am");
+        }else if(day==3&&time>=12&&time<15){
+            System.out.println("DSA(Data Structure and algorithm) 12:00Pm to 3:00Pm");
+        }else if(day==3&&time>=15&&time<16){
+            System.out.println("Rest and Chill 3:00Pm to 4:00Pm");
+        }else if(day==3&&time>=16&&time<19){
+            System.out.println("Web Development 4:00Pm to 7:00Pm");
+        }else if(day==3&&time>=19&&time<20){
+            System.out.println("Rest and Chill 7:00Pm to 8:00Pm");
+        }else if(day==3&&time>=20&&time<22){
+            System.out.println("Trading time 8:00Pm to 10:00Pm");
+        }else if(day==3&&time>=22&&time<23){
+            System.out.println("Trading and eating food 10:00Pm to 11:00Pm");
+        }else if(day==3&&time>=23&&time<24.5){
+            System.out.println("Youtube Knowledge 11Pm to 12:30Am");
+        }else if(day==3&&time>=0.50&&time<2){
+            System.out.println("Conding Practice 12:30Am to 2:00Am");
+        }else if(day==3&&time>=2&&time<10){
+            System.out.println("Rest and all other activity 2:00Am to 10:00Am");
+        }else if(day==4&&time>=10&&time<12){
+            System.out.println("Revision 10:00Am to 12:00Am");
+        }else if(day==4&&time>=12&&time<15){
+            System.out.println("DSA(Data Structure and algorithm) 12:00Pm to 3:00Pm");
+        }else if(day==4&&time>=15&&time<16){
+            System.out.println("Rest and Chill 3:00Pm to 4:00Pm");
+        }else if(day==4&&time>=16&&time<19){
+            System.out.println("Web Development 4:00Pm to 7:00Pm");
+        }else if(day==4&&time>=19&&time<20){
+            System.out.println("Rest and Chill 7:00Pm to 8:00Pm");
+        }else if(day==4&&time>=20&&time<22){
+            System.out.println("Trading time 8:00Pm to 10:00Pm");
+        }else if(day==4&&time>=22&&time<23){
+            System.out.println("Trading and eating food 10:00Pm to 11:00Pm");
+        }else if(day==4&&time>=23&&time<24.5){
+            System.out.println("Youtube Knowledge 11Pm to 12:30Am");
+        }else if(day==4&&time>=0.50&&time<2){
+            System.out.println("Conding Practice 12:30Am to 2:00Am");
+        }else if(day==4&&time>=2&&time<10){
+            System.out.println("Rest and all other activity 2:00Am to 10:00Am");
+        }else if(day==5&&time>=10&&time<12){
+            System.out.println("Revision 10:00Am to 12:00Am");
+        }else if(day==5&&time>=12&&time<15){
+            System.out.println("DSA(Data Structure and algorithm) 12:00Pm to 3:00Pm");
+        }else if(day==5&&time>=15&&time<16){
+            System.out.println("Rest and Chill 3:00Pm to 4:00Pm");
+        }else if(day==5&&time>=16&&time<19){
+            System.out.println("Web Development 4:00Pm to 7:00Pm");
+        }else if(day==5&&time>=19&&time<20){
+            System.out.println("Rest and Chill 7:00Pm to 8:00Pm");
+        }else if(day==5&&time>=20&&time<22){
+            System.out.println("Trading time 8:00Pm to 10:00Pm");
+        }else if(day==5&&time>=22&&time<23){
+            System.out.println("Trading and eating food 10:00Pm to 11:00Pm");
+        }else if(day==5&&time>=23&&time<24.5){
+            System.out.println("Youtube Knowledge 11Pm to 12:30Am");
+        }else if(day==5&&time>=0.50&&time<2){
+            System.out.println("Conding Practice 12:30Am to 2:00Am");
+        }else if(day==5&&time>=2&&time<10){
+            System.out.println("Rest and all other activity 2:00Am to 10:00Am");
+        }else{
+            System.out.println("Not a valid day no");
+        }
+    }
+}
